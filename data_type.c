8fcbe8ac3628e33844029427e1e13ae1ff1d1ad9
Tamjid8880 <123306130+Tamjid8880@users.noreply.github.com>
2023-01-29 02:04:10 +0600

    Add files via upload

---
+++ data_type.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+
+//Add the correct data type for the following variables:
+/*
+int myNum = 5;
+float myFloatNum = 5.99;
+char myLetter = 'D';
+*/
+
+//Add the correct format specifier to print the value of the following variable:
+
+char myLetter = 'D';
+printf("%c", myLetter);
+
+    return 0;
+}
